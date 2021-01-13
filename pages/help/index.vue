e2d6a34cf462310c97e9bd4b82ce1e25a2814992
闫怀堂 <468012316@qq.com>
2021-01-13 13:59:03 +0800

    初始化项目

---
+++ pages/help/index.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="">
+		<view class="u-card-wrap">
+			<u-card @click="click" @head-click="headClick" :title="title" :sub-title="subTitle" :thumb="thumb" :padding="padding" :border="border">
+				<view class="" slot="body"> 
+					<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
+						<view class="u-body-item-title u-line-2">
+							瓶身描绘的牡丹一如你初妆，冉冉檀香透过窗心事我了然，宣纸上走笔至此搁一半
+						</view>
+						<image src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg" mode="aspectFill"></image>
+					</view>
+				</view>
+			</u-card>
+			<u-card @click="click" @head-click="headClick" :title="title" :sub-title="subTitle" :thumb="thumb" :padding="padding" :border="border">
+				<view class="" slot="body"> 
+					<view class="u-body-item u-flex u-row-between u-p-b-0">
+						<view class="u-body-item-title u-line-2">
+							釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放
+						</view>
+						<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
+					</view>
+				</view>
+			</u-card>
+			<u-card @click="click" @head-click="headClick" :title="title" :sub-title="subTitle" :thumb="thumb" :padding="padding" :border="border">
+				<view class="" slot="body"> 
+					<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
+						<view class="u-body-item-title u-line-2">
+							瓶身描绘的牡丹一如你初妆，冉冉檀香透过窗心事我了然，宣纸上走笔至此搁一半
+						</view>
+						<image src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg" mode="aspectFill"></image>
+					</view>
+				</view>
+			</u-card>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '素胚勾勒出青花，笔锋浓转淡',
+				subTitle: '',//'2020-05-15',
+				thumb: '',//'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
+				padding: 20,
+				bottomSlot: true,
+				border: true
+			}
+		},
+		methods: {
+			borderChange(index) {
+				this.border = !index;
+			},
+			click(index) {
+				console.log(index);
+			},
+			headClick(index) {
+				console.log(index);
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.u-demo {
+		padding-top: 0;
+	}
+	
+	.u-body-item {
+		font-size: 32rpx;
+		color: #333;
+		padding: 20rpx 10rpx;
+	}
+		
+	.u-body-item image {
+		width: 120rpx;
+		flex: 0 0 120rpx;
+		height: 120rpx;
+		border-radius: 8rpx;
+		margin-left: 12rpx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,105 @@
+<template>
+	<view class="yueke-index">
+		<view class="yueke-swiper-area">
+			<u-toast ref="uToast"></u-toast>
+			<u-swiper @change="change" :height="250" :list="list" :title="title" :effect3d="effect3d" 
+			:indicator-pos="indicatorPos" :mode="mode" :interval="3000" @click="click"></u-swiper>
+		</view>
+		<view class="yueke-star-student">
+			<view class="item-wrap">
+				<block class="item">
+					<video class="item-video" id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+					                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+				</block>
+				<block class="item">
+					<video class="item-video" id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+					                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+				</block>
+				<block class="item">
+					<video class="item-video" id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+					                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+				</block>
+				<block class="item">
+					<video class="item-video" id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+					                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+				</block>
+			</view>
+			
+		</view>
+		
+		
+		
+		
+	</view>
+</template>
+	
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
+				title: false,
+				mode: 'round',
+				indicatorPos: 'bottomCenter',
+				effect3d: false,
+				
+			}
+		},
+		methods: {
+			titleChange(index) {
+				this.title = index == 0 ? true : false;
+			},
+			modeChange(index) {
+				this.mode = index == 0 ? 'round' : index == 1 ? 'rect' : index == 2 ? 'number' : 'none';
+			},
+			indicatorPosChange(index) {
+				this.indicatorPos = index == 0 ? 'topLeft' : index == 1 ? 'topRight' : index == 2 ? 'bottomLeft' : index == 3 ? 'bottomCenter'  : 'bottomRight';
+			},
+			effect3dChange(index) {
+				this.effect3d = index == 0 ? true : false;
+			},
+			click(index) {
+				this.$refs.uToast.show({
+					title: `点击了第${index + 1}张图片`,
+					type: 'success'
+				})
+			},
+			change(index) {
+				// console.log(index);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.yueke-star-student {
+		margin-top: 30rpx;
+		overflow-x: scroll;
+	}
+	.item-wrap {
+		width: 1400rpx;
+	}
+	.item {
+		margin: 30rpx 0;
+	}
+	.item-video {
+		margin-right: 30rpx;
+		width: 300rpx;
+		height: 200rpx;
+		border-radius: 20rpx;;
+	}
+</style>
+
